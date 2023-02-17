4fdfc783aaa34bd3925a0bed78b016518436ef3e
iori-oiso <iori.dazo@gmail.com>
2023-02-17 14:55:24 +0900

    Update: elastic-agent

--- tools/zipkin/start.bat
+++ tools/zipkin/start.bat
@@ -4,8 +4,14 @@ for /f "usebackq tokens=1,* delims==" %%a in ("env") do (
 )
 
 set SCOOP=C:\Users\%USER%\scoop
-set JAVA=%SCOOP%\apps\openjdk11\current\bin\java.exe
-set DIR=%SCOOP%\apps\zipkin\current
+
+if not exist %SCOOP%\apps\zipkin\current\zipkin-server-exec.jar copy /b %SCOOP%\apps\zipkin\current\zipkin-server-*-exec.jar %SCOOP%\apps\zipkin\current\zipkin-server-exec.jar
+
+::set STORAGE_TYPE=elasticsearch
+::set ES_HOSTS=https://localhost:9200
+::set ES_USERNAME=%ES_USERNAME%
+::set ES_PASSWORD=%ES_PASSWORD%
+::set ES_HTTP_LOGGING=BASIC
 
 :: java -jar zipkin-server-2.23.16-exec.jar
-%JAVA% -jar %DIR%\zipkin-server-%VER%-exec.jar
+%SCOOP%\apps\openjdk11\current\bin\java.exe -jar %SCOOP%\apps\zipkin\current\zipkin-server-exec.jar
